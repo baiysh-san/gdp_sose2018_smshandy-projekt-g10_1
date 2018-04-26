c8e1861a7a24fde643130e002f9b06f75ece6997
Zhanysh_Toktosunov <Zhanysh.Toktosunov.i11@fh-zwickau.de>
2018-04-26 19:32:27 +0200

    Empty class Provider

---
+++ src/smsHandy/Provider.java
@@ -0,0 +1,23 @@
+package smsHandy;
+
+import java.util.Map;
+
+public class Provider {
+    private Map<String, Integer> credits;
+    
+    public Provider() {
+        
+    }
+    
+    public boolean send(Message message) {
+        return true;
+    }
+    
+    public void register(SmsHandy smsHandy) {
+        
+    }
+    
+    public void deposit() {
+        
+    }
+}
