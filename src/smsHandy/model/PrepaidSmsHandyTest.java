6a461ec6cc52700b682d9f78eaaa716c859359d3
Zhanysh_Toktosunov <Zhanysh.Toktosunov.i11@fh-zwickau.de>
2018-05-13 17:10:55 +0200

    test test

---
+++ src/smsHandy/model/PrepaidSmsHandyTest.java
@@ -0,0 +1,29 @@
+package smsHandy.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrepaidSmsHandyTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testCanSendSms() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPayForSms() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDeposit() {
+        fail("Not yet implemented");
+    }
+
+}
