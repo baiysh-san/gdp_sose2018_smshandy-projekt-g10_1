30d4cdea2428c63fbae3d6dc844acc999835a4ec
Zhanysh_Toktosunov <Zhanysh.Toktosunov.i11@fh-zwickau.de>
2018-05-28 17:57:29 +0200

    Added fxml files and brand new class - MainApp

---
+++ src/smsHandy/MainApp.java
@@ -0,0 +1,78 @@
+package smsHandy;
+
+import java.io.IOException;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainApp extends Application{
+    private Stage stage; 
+    private BorderPane rootPane;
+    @Override
+    public void start(Stage stage) throws Exception {
+        // TODO Auto-generated method stub
+        this.stage = stage;
+        showRootPanel();
+        showMessageOverview();
+        showProviderOverview();
+        showHandyOverview();
+    }
+    
+    public void showRootPanel() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootPanel.fxml"));
+            rootPane = (BorderPane)loader.load();
+            Scene scene  = new Scene(rootPane);
+            stage.setScene(scene);
+            stage.show();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showProviderOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/ProviderOverview.fxml"));
+            AnchorPane providerOverview = (AnchorPane) loader.load();
+            rootPane.setLeft(providerOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showHandyOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/HandyOverview.fxml"));
+            AnchorPane handyOverview = (AnchorPane) loader.load();
+            rootPane.setCenter(handyOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showMessageOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/MessageOverview.fxml"));
+            VBox messageOverview = (VBox) loader.load();
+            rootPane.setRight(messageOverview);
+           
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
