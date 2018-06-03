f4b9fa3bbae931b7eaa2f4d01449e3ca9175362c
zht17i11 <Zhanysh.Toktosunov.i11@fh-zwickau.de>
2018-06-03 23:26:35 +0200

    Added StringProperties AAAAA! Dumb javafx!!!

--- src/smsHandy/model/Message.java
+++ src/smsHandy/model/Message.java
@@ -4,6 +4,9 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import javafx.beans.property.SimpleStringProperty;
+
+import javafx.beans.property.StringProperty;
 /**
  * A Message that can be sent via SMSHandy.
  */
@@ -12,15 +15,25 @@ public class Message {
     private Date date;
     private String from;
     private String to;
+    private final StringProperty contentProperty;
+    private final StringProperty toProperty;
+    private final StringProperty fromProperty;
+    private final StringProperty datePropery;
     
-    public Message() {   
-    }
+    //public Message() {   
+    //}
     
     public Message(String content, String to, String from, Date date) {
         this.content = content;
         this.to = to;
         this.from = from;
         this.date = date;
+        this.contentProperty = new SimpleStringProperty(content);
+        this.toProperty = new SimpleStringProperty(to);
+        this.fromProperty = new SimpleStringProperty(from);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        String dateString = dateFormat.format(date);
+        this.datePropery = new SimpleStringProperty(dateString);
     }
     
     public String getContent() {
@@ -55,6 +68,18 @@ public class Message {
                 + "From: " + from + " To: " + to + "\n"
                 + content;
         return message;
+    }
+    public StringProperty getContentProperty() {
+        return contentProperty;
+    }
+    public StringProperty getDatePropery() {
+        return datePropery;
+    }
+    public StringProperty getFromProperty() {
+        return fromProperty;
+    }
+    public StringProperty getToProperty() {
+        return toProperty;
     }
      
 }
