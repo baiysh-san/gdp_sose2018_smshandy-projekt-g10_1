1949ba0f63fc00677bb678a72cfe595dfc13a611
zht17i11 <Zhanysh.Toktosunov.i11@fh-zwickau.de>
2018-06-04 05:00:56 +0200

    Fixed some bugs in DepositController

--- src/smsHandy/view/DepositController.java
+++ src/smsHandy/view/DepositController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import smsHandy.MainApp;
 import smsHandy.model.PrepaidSmsHandy;
 
 /**
@@ -21,12 +22,20 @@ public class DepositController {
     private Button cancelButton;
     
     PrepaidSmsHandy handy;
+    private MainApp mainApp;
     
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+    /**
+     * set Handy
+     * @param handy
+     */
     public void setSettings(PrepaidSmsHandy handy) {
         this.handy = handy;
         depositField.textProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, 
+            public void changed(ObservableValue<? extends String> observable, String oldValue, //Typing only numbers
                 String newValue) {
                 if (!newValue.matches("\\d*")) {
                     depositField.setText(newValue.replaceAll("[^\\d]", ""));
@@ -34,18 +43,30 @@ public class DepositController {
             }
         });
     }
-    
+    /**
+     * Reaction to uploadButton
+     */
     @FXML
     private void handleUploadButton() {
         try {
-            handy.deposit(Integer.parseInt(depositField.getText()));
-            handleCancelButton();
+            int amount = Integer.parseInt(depositField.getText());
+            if (amount > 0) {
+                handy.deposit(Integer.parseInt(depositField.getText()));
+                handleCancelButton();
+            } else {
+                mainApp.showAlert("Amount must be greater than 0!", "Please give me you money!!1");
+                //throw new Exception("Amount must be greater than 0");
+            }
+            
         } catch (NumberFormatException e) {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    /**
+     * Reaction to cancelButton
+     */
     @FXML
     private void handleCancelButton() {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
